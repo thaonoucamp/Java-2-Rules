3ab4bde297196b6b3e512baa0feae42f7868a729
thaonoucamp <dangxuanthaoht@gamil.com>
2021-01-17 16:40:05 +0700

    add

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello Java");
+
+        // Ten bien;
+        int aB = 10;
+        int ab = 10;
+        int $a = 10;
+        int _x = 10;
+
+        // Ten Ham dong tu;
+        int chieuCao = 5;
+
+        // Ten HANG_SO;
+        final int HANG_SO = 10;
+
+        // Comment;
+        /*
+
+         */
+    }
+}
